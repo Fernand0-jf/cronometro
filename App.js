4043349f275e113e690e0d591c819d5ebcf184fb
Fernand0-jf <rubronm@hotmail.com>
2024-04-20 18:50:40 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {View, Text,StyleSheet} from 'react-native';
+
+export default function App(){
+  return(
+    <View>
+      <Text>ola, tudo bem?</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    felx:1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+
+});
